13686fa9b0b0b5aa17f1bc1296ccd00a5e6bbc68
sumanhong <suman0204@naver.com>
2023-11-13 09:53:29 +0900

    Initial Commit

---
+++ SeSACSwiftUI/SeSACSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeSACSwiftUIApp.swift
+//  SeSACSwiftUI
+//
+//  Created by 홍수만 on 2023/11/13.
+//
+
+import SwiftUI
+
+@main
+struct SeSACSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SeSACSwiftUIUITests/SeSACSwiftUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SeSACSwiftUIUITestsLaunchTests.swift
+//  SeSACSwiftUIUITests
+//
+//  Created by 홍수만 on 2023/11/13.
+//
+
+import XCTest
+
+final class SeSACSwiftUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
